eb7977abd754e99bf0a8f507885c46a59a6d8fc4
centos7 <centos7@localhost>
2017-12-30 13:03:00 +0800

    delete json.hpp add tplengine.hpp

---
+++ ngx_http_hi_module/include/tplengine.hpp
@@ -0,0 +1,53 @@
+#ifndef TPLENGINE_HPP
+#define TPLENGINE_HPP
+
+
+#include <sstream>
+#include <string>
+
+#include "NLTemplate.hpp"
+#include "request.hpp"
+#include "response.hpp"
+
+namespace hi {
+
+    class tplengine {
+    public:
+
+        tpl_engine() :
+        loader(), tpl(loader) {
+        }
+        virtual ~tpl_engine() = default;
+
+        void render(hi::response& res) {
+            std::stringbuf buf;
+            std::ostream os(&buf);
+            this->tpl.render(os);
+            res.content = std::move(buf.str());
+            res.status = 200;
+        }
+
+        tplengine& load(const std::string& path) {
+            this->tpl.load(path);
+            return *this;
+        }
+
+        tplengine& set(const std::string& key, const std::string& value) {
+            this->tpl.set(key, value);
+            return *this;
+        }
+
+        NL::Template::Template& engine() {
+            return this->tpl;
+        }
+
+
+
+    private:
+        NL::Template::LoaderFile loader;
+        NL::Template::Template tpl;
+    };
+}
+
+#endif /* TPLENGINE_HPP */
+
