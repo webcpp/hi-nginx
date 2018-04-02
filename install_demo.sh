e9e5ea33ce7e8ad0fb0aafa1f38bc7ecb7931dbb
pangpang <pangpang@hi-nginx.com>
2018-04-03 06:24:01 +0800

    change readme

--- install_demo.sh
+++ install_demo.sh
@@ -13,7 +13,6 @@ if ! test -f Makefile;then
                 --with-http-hi-python-version=3                     \
                 --with-http-hi-lua-version=lua                      \
                 --add-module=ngx_http_hi_module                     \
-                --add-module=3rd/nginx-http-concat-1.2.2            \
                 --add-module=3rd/nchan-1.1.14                       \
                 --add-module=3rd/nginx-rtmp-module-1.2.1            
 else
