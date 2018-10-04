4e0be7f0343106e369019f1b9bad3b11a15a02d7
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2018-10-04 15:10:44 +0800

    update nginx version to 1.15.5

--- install_demo.sh
+++ install_demo.sh
@@ -6,10 +6,10 @@ if ! test -f Makefile;then
                 --with-http_v2_module                               \
                 --prefix=/usr/local/nginx                           \
                 --enable-http-hi-cpp=YES                            \
-                --enable-http-hi-python=NO                         \
-                --enable-http-hi-lua=NO                            \
-                --enable-http-hi-java=NO                           \
-                --enable-http-hi-php=NO                            \
+                --enable-http-hi-python=YES                         \
+                --enable-http-hi-lua=YES                            \
+                --enable-http-hi-java=YES                           \
+                --enable-http-hi-php=YES                            \
                 --with-http-hi-python-version=python-3.5               \
                 --with-http-hi-lua-version=lua5.1                   \
                 --add-module=ngx_http_hi_module                     \
