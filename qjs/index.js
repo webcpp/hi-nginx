8dc04e46a2239b5cbb0c6ccfe49c19739cb52288
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2020-07-26 17:51:55 +0800

    update qjs example

--- qjs/index.js
+++ qjs/index.js
@@ -4,9 +4,9 @@
 //import * as hash from "hash";
 // import { crypto } from "crypto";
 import * as hi from "hi";
-import test from "./test/base_test.mjs"
+//import test from "./test/base_test.mjs"
 // import test from "./test/curl_test.mjs"
-// import test from "./test/route_test.mjs"
+import test from "./test/route_test.mjs"
 // import test from "./test/crypto_test.mjs"
 // import test from "./test/hash_test.mjs"
 // import test from "./test/tpl_test.mjs"
--- qjs/test/route_test.mjs
+++ qjs/test/route_test.mjs
@@ -1,16 +1,18 @@
 import * as hi from "hi";
-import { crypto } from "crypto";
+import {
+  crypto
+} from "crypto";
 import route from "./lib/route.mjs"
 
 var r = route.get_instance();
 
-r.get('^/$', function (m, param) {
+r.get('^/test$', function (m, param) {
   m.status(200);
   m.header('Content-Type', 'text/plain;charset=utf-8');
   m.content('hello,world\n');
 });
 
-r.get('^/(.*)/?$', function (m, param) {
+r.get('^/hello/(.*)/?$', function (m, param) {
   m.header('Content-Type', 'text/plain;charset=UTF8')
   m.content(m.method() + '\n' + m.uri() + '\n' + param.toString())
   m.status(200)
